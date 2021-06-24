ca30dbb72080a73c160938788b25844a2ba070e6
Rishiraj0100 <73101003+Rishiraj0100@users.noreply.github.com>
2021-06-24 12:08:14 +0530

    Delete webeye directory

--- webeye/__init__.py
+++
@@ -1,8 +0,0 @@
-from .master import *
-
-__version__ = "0.1.0"
-__author__ = "Zaeem Technical"
-__all__ = [
-    "subenum", "dns", "whois", "grab", "portscan", "cloudflare", "__version__",
-    "__author__", "Session"
-]
