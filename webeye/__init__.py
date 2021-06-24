8ba392f65dc1c6293b3e7a2db272fe99b67f3ea7
Rishiraj0100 <73101003+Rishiraj0100@users.noreply.github.com>
2021-06-24 12:09:33 +0530

    Add files via upload

---
+++ webeye/__init__.py
@@ -0,0 +1,7 @@
+from .webeye import *
+
+__name__ = "webeye"
+__version__ = "0.1.0"
+__author__ = "Zaeem Technical"
+__helper__ = "Rishi Raj"
+__all__ = ["Webeye", "__name__", "__version__", "__author__", "__helper__"]
