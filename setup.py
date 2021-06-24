99877e42ccaad13774e3164b6fcd97d363e64faf
Zaeem Technical <duranizaeem@gmail.com>
2021-06-24 11:58:16 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+  name='webeye',
+  version='0.1',
+  description='A best Powerful module for making ethical hacking tools easier',
+  url='https://github.com/Zaeem20/webeye',
+  author="Zaeem Technical",
+  author_email='business@zaeemtechnical.ml',
+  license='MIT',
+  classifiers=["License :: OSI Approved :: MIT License","Programming Language :: Python :: 3.8",],
+  python_requires=">=3.8",
+  install_requires=['aiohttp >= 3.7'],
+  keywords="webeye red_hawk nikto webrecon recondog",
+  data_files=None
+)
