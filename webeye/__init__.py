ae8ba7765821fddd1f7fb241e822d4a6a0c7b116
Rishiraj0100 <73101003+Rishiraj0100@users.noreply.github.com>
2021-06-25 12:48:19 +0530

    Delete webeye directory

--- webeye/__init__.py
+++
@@ -1,9 +0,0 @@
-from .webeye import *
-
-__name__ = "webeye"
-__version__ = "0.1.0"
-__author__ = "Zaeem Technical"
-__helper__ = "Rishi Raj"
-__github__ = "https://github.com/Zaeem20/webeye/"
-__licence__ = "MIT"
-__all__ = ["Webeye", "__name__", "__version__", "__author__", "__helper__","__github__","__license__"]
