790491743e742a23a16083bb47c80df81928bd8a
Zaeem Technical <duranizaeem@gmail.com>
2021-06-25 16:54:52 +0530

    Update __init__.py

--- webeye/__init__.py
+++ webeye/__init__.py
@@ -1,7 +1,7 @@
 from .webeye import *
 
 __name__ = "webeye"
-__version__ = "1.0.0"
+__version__ = "0.2.2"
 __author__ = "Zaeem Technical"
 __helper__ = "Rishi Raj"
 __github__ = "https://github.com/Zaeem20/webeye/"
