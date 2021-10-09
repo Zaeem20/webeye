839ccf081d596becfa4bb287bf7b6075229473c9
Zaeem Technical <duranizaeem@gmail.com>
2021-10-09 18:45:05 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,19 +4,19 @@ with open("README.md", "r", encoding="utf-8") as f:
     readme = f.read()
 
 setup(
-  name='webeye',
-  version='2.0.3',
-  long_description=readme,
-  long_description_content_type="text/markdown",
-  description='A best Powerful module for making ethical hacking tools easier',
-  url='https://github.com/Zaeem20/webeye',
-  author="Zaeem Technical",
-  author_email='business@zaeemtechnical.ml',
-  license='MIT',
-  classifiers=["License :: OSI Approved :: MIT License","Programming Language :: Python :: 3.8",],
-  python_requires=">=3.8",
-  install_requires=['requests >= 2'],
-  keywords="webeye red_hawk nikto webrecon recondog",
-  packages=find_packages(exclude=["docs","test"]),
-  data_files=None
+    name='webeye',
+    version='2.1.1',
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    description='A best Powerful module for making ethical hacking tools easier',
+    url='https://github.com/Zaeem20/webeye',
+    author="Zaeem Technical",
+    author_email='business@zaeemtechnical.ml',
+    license='MIT',
+    classifiers=["License :: OSI Approved :: MIT License","Programming Language :: Python :: 3.8",],
+    python_requires=">=3.8",
+    install_requires=['requests >= 2'],
+    keywords="webeye red_hawk nikto webrecon recondog",
+    packages=find_packages(exclude=["docs","tests"]),
+    data_files=None
 )
