bf691629c82546719a258295e56593550ad66ae9
Zaeem Technical <duranizaeem@gmail.com>
2021-10-09 18:41:20 +0530

    Update __init__.py

--- webeye/__init__.py
+++ webeye/__init__.py
@@ -1,9 +1,10 @@
 from .webeye import *
 
 __name__ = "webeye"
-__version__ = "2.0.3"
+__version__ = "2.1.1"
 __author__ = "Zaeem Technical"
 __helper__ = "Rishi Raj"
 __github__ = "https://github.com/Zaeem20/webeye/"
 __licence__ = "MIT"
 __all__ = ["Webeye", "__name__", "__version__", "__author__", "__helper__","__github__","__license__"]
+
