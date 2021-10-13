e9c96e6bd6d36c5afba9eecabe530641593b3671
RishiDev <73101003+Rishiraj0100@users.noreply.github.com>
2021-10-13 19:13:57 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 setup(
     name='webeye',
-    version='2.1.6',
+    version='2.1.7',
     long_description=readme,
     long_description_content_type="text/markdown",
     description='A Best Powerful module for making ethical hacking tools easier',
@@ -13,6 +13,11 @@ setup(
     author="Zaeem Technical",
     author_email='business@zaeemtechnical.ml',
     license='MIT',
+    entry_points={
+        "console_scripts": [
+            "webeye = webeye.cli:main"
+        ]
+    },
     classifiers=["License :: OSI Approved :: MIT License","Programming Language :: Python :: 3.8",],
     python_requires=">=3.8",
     install_requires=['requests >= 2','httpx == 0.19.0'],
