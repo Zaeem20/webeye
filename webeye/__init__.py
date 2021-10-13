8f6b7bb4bb9ff3230477ea601709d63af289c0ad
RishiDev <73101003+Rishiraj0100@users.noreply.github.com>
2021-10-13 19:14:20 +0530

    Update __init__.py

--- webeye/__init__.py
+++ webeye/__init__.py
@@ -1,7 +1,7 @@
 from .core import *
 
 __name__ = "webeye"
-__version__ = "2.1.6"
+__version__ = "2.1.7"
 __author__ = "Zaeem Technical"
 __github__ = "https://github.com/Zaeem20/webeye/"
 __licence__ = "MIT"
