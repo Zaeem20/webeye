eb99700d5c4149f875bb19240c2e20ad162fa94f
Zaeem20 <duranizaeem@gmail.com>
2021-10-15 20:04:09 +0530

    added

--- webeye/__init__.py
+++ webeye/__init__.py
@@ -1,7 +1,7 @@
 from .core import *
 
 __name__ = "webeye"
-__version__ = "2.2.2"
+__version__ = "2.2.3"
 __author__ = "Zaeem Technical"
 __helper__ = "Rishi Raj"
 __github__ = "https://github.com/Zaeem20/webeye/"
